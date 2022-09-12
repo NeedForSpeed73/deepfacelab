55934e99bb3ab1f98295a2c6b41fccc6f083f2cf
unknown <need_for_speed@libero.it>
2022-09-12 16:55:45 +0200

    no .py

--- scripts/1_test_GPU.sh
+++ scripts/1_test_GPU.sh
@@ -1,1 +1,10 @@
-python DeepFaceLab/test_GPU.py
+#!/bin/bash
+
+#python DeepFaceLab/test_GPU.py
+
+function test_GPU {
+	python3 - << END
+import tensorflow as tf
+print('Num GPUs Available: ', len(tf.config.list_physical_devices('GPU')))
+END
+
