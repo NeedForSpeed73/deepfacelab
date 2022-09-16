bfcc09744bc46c03695a88072ea248b5a30162bd
unknown <need_for_speed@libero.it>
2022-09-16 12:45:03 +0200

    Update 4_download_workspace_GDrive.sh

--- scripts/4_download_workspace_GDrive.sh
+++ scripts/4_download_workspace_GDrive.sh
@@ -3,7 +3,7 @@
 if [ "$#" -ne 1 ]; then
 	echo "Usage: 4_download_workspace_GDrive.sh <file_id> from GDrive public link."
 else
-	gdown $1
+	./local/bin/gdown $1
 	tar -xzf workspace.tar.gz
 	rm workspace.tar.gz
 fi
