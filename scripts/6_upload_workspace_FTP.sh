fb7db6ad8ad111afa1a6c9d3bad67262a419ce3b
unknown <need_for_speed@libero.it>
2022-09-16 13:25:54 +0200

    Update 6_upload_workspace_FTP.sh

--- scripts/6_upload_workspace_FTP.sh
+++ scripts/6_upload_workspace_FTP.sh
@@ -3,13 +3,13 @@
 DIR=$HOME/deepfacelab/workspace
 
 if [ "$#" -ne 2 ]; then
-	echo "Usage: 6_upload_workspace_FTP ftp.youserver.com directory"
+	echo "Usage: 6_upload_model ftp.youserver.com directory"
 else 
 	if [ -d "$DIR" ]; then
 		if [ "$(ls -A $DIR)" ]; then
-			tar -czf workspace.tar.gz workspace/*
-			ncftpput -u emilio ftp://$1 $2 workspace.tar.gz
-			rm workspace.tar.gz
+			tar -czf model.tar.gz workspace/model/*
+			ncftpput -u emilio ftp://$1 $2 model.tar.gz
+			rm model.tar.gz
 			echo "Done."
 		else
 			echo "$DIR is empty"
