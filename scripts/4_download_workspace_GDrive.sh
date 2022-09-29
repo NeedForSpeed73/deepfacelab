2d797a1253e551f5681811b6be2206eb482528d3
NeedForSpeed73 <need_for_speed@libero.it>
2022-09-29 22:16:08 +0200

    Update 4_download_workspace_GDrive.sh

--- scripts/4_download_workspace_GDrive.sh
+++ scripts/4_download_workspace_GDrive.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ "$#" -ne 1 ]; then
-	echo "Usage: 4_download_workspace_GDrive.sh <file_id> from GDrive public link."
+	echo "Usage: 4_download_workspace_GDrive.sh <file_id> from GDrive link."
 else
-	gdown $1
-	tar -xzf workspace.tar.gz
-	rm workspace.tar.gz
+	./gdrive $1
+	7z x workspace.7z
+	rm workspace.7z
 fi
