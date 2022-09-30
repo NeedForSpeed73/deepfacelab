d823e9c02d4961baa70301ed3c17b0225ba46af8
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-01 01:20:45 +0200

    Update 1_Install_Miniconda.sh

--- 1_Install_Miniconda.sh
+++ 1_Install_Miniconda.sh
@@ -5,10 +5,3 @@
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
 sh ./Miniconda3-latest-Linux-x86_64.sh
 rm Miniconda3-latest-Linux-x86_64.sh
-
-echo 'export -f conda' >> ~/.bashrc
-echo 'export -f __conda_activate' >> ~/.bashrc
-echo 'export -f __conda_reactivate' >> ~/.bashrc
-echo 'export -f __add_sys_prefix_to_path' >> ~/.bashrc
-echo 'export -f __conda_hashr' >> ~/.bashrc
-echo 'export -f __conda_exe' >> ~/.bashrc
