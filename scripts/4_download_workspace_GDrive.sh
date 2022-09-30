becb54dc680840ca2f6e0111b3c7bae58f887b31
NeedForSpeed73 <need_for_speed@libero.it>
2022-09-30 03:09:32 +0200

    Update 4_download_workspace_GDrive.sh

--- scripts/4_download_workspace_GDrive.sh
+++ scripts/4_download_workspace_GDrive.sh
@@ -3,7 +3,7 @@
 if [ "$#" -ne 1 ]; then
 	echo "Usage: 4_download_workspace_GDrive.sh <file_id> from GDrive link."
 else
-	./gdrive $1
+	./gdrive download $1
 	7z x workspace.7z
 	rm workspace.7z
 fi
