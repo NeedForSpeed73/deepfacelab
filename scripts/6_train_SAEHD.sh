1a569d19392b10575ac44a5d5e133326cab87c5f
NeedForSpeed73 <need_for_speed@libero.it>
2022-09-30 21:43:11 +0200

    Update 6_train_SAEHD.sh

--- scripts/6_train_SAEHD.sh
+++ scripts/6_train_SAEHD.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #
 
-if [ "$#" -ne 0 ] | [ "$#" -ne 1 ]; then
+if [ "$#" -gt 1 ]; then
         echo "Usage: 6_train_SAEHD.sh [--no-preview]"
 else
-  if [ $1 != "--no-preview" ]; then
-    echo "Usage: 6_train_SAEHD.sh [--no-preview]"
-  else
-    python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/d>  fi
+  if [ "$#" -eq 0 ]; then
+         python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --pretraining-data-dir pretrain --model-dir workspace/model --model "SAEHD"
+  else if [ $1 != "--no-preview" ]; then
+                echo "Usage: 6_train_SAEHD.sh [--no-preview]"
+        else
+                python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --pretraining-data-dir pretrain --model-dir workspace/model --model "SAEHD" --no-preview
+       fi
+  fi
 fi
