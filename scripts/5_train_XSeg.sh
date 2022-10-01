415de6a872565d250a0c33ed3d4d673c3ba5c20e
unknown <need_for_speed@libero.it>
2022-10-01 13:29:45 +0200

    Latest

--- scripts/5_train_XSeg.sh
+++ scripts/5_train_XSeg.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+#
 
-python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --model-dir workspace/model --model "XSeg" --no-preview
+if [ "$#" -gt 1 ]; then
+        echo "Usage: 5_train_XSeg.sh [--no-preview]"
+else
+	if [ "$#" -eq 0 ]; then
+		python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --model-dir workspace/model --model "XSeg"
+	else if [ $1 != "--no-preview" ]; then
+			echo "Usage: 5_train_XSeg.sh [--no-preview]"
+		else
+			python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --model-dir workspace/model --model "XSeg" --no-preview
+		fi
+	fi
+fi
--- scripts/6_train_SAEHD.sh
+++ scripts/6_train_SAEHD.sh
@@ -4,12 +4,12 @@
 if [ "$#" -gt 1 ]; then
         echo "Usage: 6_train_SAEHD.sh [--no-preview]"
 else
-  if [ "$#" -eq 0 ]; then
-         python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --pretraining-data-dir pretrain --model-dir workspace/model --model "SAEHD"
-  else if [ $1 != "--no-preview" ]; then
-                echo "Usage: 6_train_SAEHD.sh [--no-preview]"
+	if [ "$#" -eq 0 ]; then
+		python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --pretraining-data-dir pretrain --model-dir workspace/model --model "SAEHD"
+	else if [ $1 != "--no-preview" ]; then
+			echo "Usage: 6_train_SAEHD.sh [--no-preview]"
         else
-                python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --pretraining-data-dir pretrain --model-dir workspace/model --model "SAEHD" --no-preview
-       fi
-  fi
+            python DeepFaceLab/main.py train --training-data-src-dir workspace/data_src/aligned --training-data-dst-dir workspace/data_dst/aligned --pretraining-data-dir pretrain --model-dir workspace/model --model "SAEHD" --no-preview
+        fi
+	fi
 fi
