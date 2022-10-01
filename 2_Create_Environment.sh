4ec4c5e55701e58d176c7288e2de9473c2ea36bc
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-01 13:04:35 +0200

    Update 2_Create_Environment.sh

--- 2_Create_Environment.sh
+++ 2_Create_Environment.sh
@@ -7,7 +7,6 @@ tar -xzf gdrive_2.1.1_linux_386.tar.gz
 rm gdrive_2.1.1_linux_386.tar.gz
 sudo -S apt update
 sudo apt -y install p7zip-full
-sudo apt-get -y clean
 
 CONDA_BASE=$(conda info --base)
 
@@ -19,12 +18,15 @@ conda activate deepfacelab
 conda info --envs #check if env is activated
 git clone --depth 1 https://github.com/iperov/DeepFaceLab.git
 python -m pip install --no-input -r ./DeepFaceLab/requirements-cuda.txt
-conda clean -y -a
 
 # Fix Permissions and libraries
 chmod +x scripts/*
 ln -s libcusolver.so.11.1.2.109 $CONDA_PREFIX_1/envs/deepfacelab/lib/libcusolver.so.10
 ln -s libcusolver.so.11.1.2.109 $CONDA_PREFIX_1/pkgs/cudatoolkit-11.3.1-h2bc3f7f_2/lib/libcusolver.so.10
+
+# Clean packagers
+sudo apt-get -y clean
+conda clean -y -a
 
 # Banner Message
 echo
