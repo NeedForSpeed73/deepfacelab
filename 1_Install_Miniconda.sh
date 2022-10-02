b4002937934b8b4e01481dda45acaebbc2fd8f0a
unknown <need_for_speed@libero.it>
2022-10-02 14:44:28 +0200

    verbose

--- 1_Install_Miniconda.sh
+++ 1_Install_Miniconda.sh
@@ -2,6 +2,16 @@
 #
 
 # Install Miniconda
+printf %"s\n" "" "* Downloading Miniconda3-latest" ""
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+
+printf %"s\n" "" "* Installing Miniconda3-latest" ""
 sh ./Miniconda3-latest-Linux-x86_64.sh
+
+printf %"s\n" "" "* Cleaning Up" ""
 rm Miniconda3-latest-Linux-x86_64.sh
+
+# Banner Message
+printf %"s\n" "" "***************************************************************"
+printf %"s\n" "* NOW LOGOUT (CLOSE) THIS SHELL AND RE-OPEN TO ACTIVATE CONDA *"
+printf %"s\n" "***************************************************************" ""
