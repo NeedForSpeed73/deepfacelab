648c7f36a534fb1dd670c6cdd87363dc0da691a4
unknown <need_for_speed@libero.it>
2022-10-02 14:30:38 +0200

    model.id

--- scripts/7_update_model_GDrive.sh
+++ scripts/7_update_model_GDrive.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 DIR=$HOME/deepfacelab/workspace/model
-
-if [ "$#" -ne 1 ]; then
-	echo "Usage: 7_upload_model_GDrive.sh model.7z file ID on GDrive"
+	
+if [ "$#" -ge 1 ]; then
+	echo "Usage: 7_upload_model_GDrive.sh model.7z [file ID on GDrive]"
 else 
 	if [ -d "$DIR" ]; then
 		if [ "$(ls -A $DIR)" ]; then
 			7z a model.7z workspace/model/*
-			./gdrive update $1 model.7z
+			if [ "$#" -eq 1 ]; then
+				MODEL_ID=$1
+			else
+				MODEL_ID=$(cat model.id)
+			./gdrive update $MODEL_ID model.7z
 			rm model.7z
 			echo "Done."
 		else
