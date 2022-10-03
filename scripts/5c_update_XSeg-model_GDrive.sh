b7b8b59b9feedeacdf401121918d69d33e74ca60
unknown <need_for_speed@libero.it>
2022-10-03 22:11:05 +0200

    separate models

---
+++ scripts/5c_update_XSeg-model_GDrive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIR=$HOME/deepfacelab/workspace/model
+
+if [ "$#" -gt 1 ]; then
+	printf %"s\n" "Usage: 5c_upload_XSeg-model_GDrive.sh model_XSeg.7z [file ID on GDrive]"
+else 
+	if [ -d "$DIR" ]; then
+		if [ "$(ls -A $DIR)" ]; then
+			7z a model_XSeg.7z workspace/model/XSeg_*
+			if [ "$#" -eq 1 ]; then
+				MODEL_ID=$1
+				printf %"s" "Using given id: "
+				printf %"s\n" $MODEL_ID
+
+			else
+				MODEL_ID=$(cat model.id)
+				printf %"s" "Using id: " $MODEL_ID
+				printf %"s\n" " from 'model.id' file"
+			fi
+			./gdrive update $MODEL_ID model_XSeg.7z
+			rm model_XSeg.7z
+			printf %"s\n" "Done."
+		else
+			printf %"s\n" "$DIR is empty"
+		fi
+	else
+		printf %"s\n" "$DIR  does not exist."
+	fi  
+fi
---
+++ scripts/7_update_SAEHD-model_GDrive.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+DIR=$HOME/deepfacelab/workspace/model
+
+if [ "$#" -gt 1 ]; then
+	printf %"s\n" "Usage: 7_upload_SAEHD-model_GDrive.sh model_SAEHD.7z [file ID on GDrive]"
+else 
+	if [ -d "$DIR" ]; then
+		if [ "$(ls -A $DIR)" ]; then
+			7z a model_SAEHD.7z workspace/model/*SAEHD_*
+			if [ "$#" -eq 1 ]; then
+				MODEL_ID=$1
+				printf %"s" "Using given id: "
+				printf %"s\n" $MODEL_ID
+
+			else
+				MODEL_ID=$(cat model.id)
+				printf %"s" "Using id: " $MODEL_ID
+				printf %"s\n" " from 'model.id' file"
+			fi
+			./gdrive update $MODEL_ID model_SAEHD.7z
+			rm model_SAEHD.7z
+			printf %"s\n" "Done."
+		else
+			printf %"s\n" "$DIR is empty"
+		fi
+	else
+		printf %"s\n" "$DIR  does not exist."
+	fi  
+fi
