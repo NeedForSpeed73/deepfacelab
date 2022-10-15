54d2c2573116fba4883fde3c3970f11d4a0fc373
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-15 13:42:47 +0200

    Update 4b_download_optional_data_dst_GDrive.sh

--- scripts/4b_download_optional_data_dst_GDrive.sh
+++ scripts/4b_download_optional_data_dst_GDrive.sh
@@ -10,8 +10,8 @@ else
 		touch $DATA_DST_ID_FILE
 		echo $DATA_DST_ID > $DATA_DST_ID_FILE
 	fi
-	# ./gdrive download $DATA_DST_ID
-	# N_FILES=`unzip -l data_dst.zip | tail -n 1 | xargs echo -n | cut -d' ' -f2`
-	# unzip -o data_dst.zip | tqdm --desc extracted --unit files --unit_scale --total $N_FILES > /dev/null
-	# rm data_dst.zip
+	 ./gdrive download $DATA_DST_ID
+	 N_FILES=`unzip -l data_dst.zip | tail -n 1 | xargs echo -n | cut -d' ' -f2`
+	 unzip -o data_dst.zip | tqdm --desc extracted --unit files --unit_scale --total $N_FILES > /dev/null
+	 rm data_dst.zip
 fi
