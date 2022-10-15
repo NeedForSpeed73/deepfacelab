920ddcf6f89f4eb0bcf85cf665328c7873f8b430
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-15 16:29:52 +0200

    Update 4b_download_optional_data_dst_GDrive.sh

--- scripts/4b_download_optional_data_dst_GDrive.sh
+++ scripts/4b_download_optional_data_dst_GDrive.sh
@@ -12,8 +12,8 @@ else
 	else
 		DATA_DST_ID=$(<$DATA_DST_ID_FILE)
 	fi
-	FREE_SPACE=$(df -H | grep -wE '/' | awk -v N=4 '{print substr($N, 1, length($N)-1)}' <<<"${NUM} ${MEGA})
-	FILE_SIZE=$(./gdrive info $DATA_DST_ID | grep Size: | awk -v N=2 '{print $N}' <<<"${NUM} ${MEGA})
+	FREE_SPACE=$(df -H | grep -wE '/' | awk -v N=4 '{print substr($N, 1, length($N)-1)}' <<<"${NUM} ${MEGA}")
+	FILE_SIZE=$(./gdrive info $DATA_DST_ID | grep Size: | awk -v N=2 '{print $N}' <<<"${NUM} ${MEGA}")
 	if [ $($FREESPACE > 2.5 * $FILE_SIZE) ]; then
 	 	./gdrive download $DATA_DST_ID
 	 	N_FILES=`unzip -l data_dst.zip | tail -n 1 | xargs echo -n | cut -d' ' -f2`
