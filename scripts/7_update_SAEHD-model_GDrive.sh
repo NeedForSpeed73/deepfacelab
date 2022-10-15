e6fe7d82216bec77a412cb5ef8fb4b4bd203831b
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-15 14:47:16 +0200

    Update and rename 7_upload_SAEHD-model_GDrive.sh to 7_update_SAEHD-model_GDrive.sh

--- scripts/7_update_SAEHD-model_GDrive.sh
+++ scripts/7_update_SAEHD-model_GDrive.sh
@@ -12,19 +12,26 @@ else
 			if [ -f "$MODEL_SAEHD_ID_FILE" ]; then
 				MODEL_SAEHD_ID=$(<$MODEL_SAEHD_ID_FILE)
 			else
-				read -e -p "Enter <model_SAEHD.zip> Google Drive ID: " MODEL_SAEHD_ID
+				read -e -p "Enter <model_SAEHD.zip> Google Drive ID or "new" for new file: " MODEL_SAEHD_ID
 				printf %"s\n"
 			fi
 			printf %"s\n" "Creating model_SAEHD.zip"
 			N_FILES=`ls -1q workspace/model/*SAEHD_* | wc -l`
 			zip model_SAEHD.zip workspace/model/*SAEHD_* | tqdm --desc added --unit files --unit_scale --total $N_FILES > /dev/null
-			./gdrive update $MODEL_SAEHD_ID model_SAEHD.zip
+			if [[ MODEL_SAEHD_ID == "new" ]]; then
+				printf %"s\n" "Creating new GDrive file model_SAEHD.zip in main directory."
+				./gdrive upload model_SAEHD.zip
+				
+			else
+				printf %"s\n" "Updating GDrive ID: " $MODEL_SAEHD_ID
+				./gdrive update $MODEL_SAEHD_ID model_SAEHD.zip
+			fi
 			rm model_SAEHD.zip
-			printf %"s\n" "Done."
 		else
 			printf %"s\n" "$MODEL_DIR is empty"
 		fi
 	else
 		printf %"s\n" "$MODEL_DIR does not exist."
 	fi  
+	printf %"s\n" "Done."
 fi
