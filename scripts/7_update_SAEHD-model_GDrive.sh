97d59911fea3f16f7aa5bf65f3d5ee432f4a7720
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-15 14:19:01 +0200

    Update 7_update_SAEHD-model_GDrive.sh

--- scripts/7_update_SAEHD-model_GDrive.sh
+++ scripts/7_update_SAEHD-model_GDrive.sh
@@ -7,7 +7,7 @@ if [ "$#" -gt 0 ]; then
 	printf %"s\n" "Usage: 7_upload_SAEHD-model_GDrive.sh"
 else 
 	if [ -d "$MODEL_DIR" ]; then
-		if [ "$(ls -A $DIR)" ]; then
+		if [ "$(ls -A $MODEL_DIR)" ]; then
 			printf %"s\n" "Creating model_SAEHD.zip"
 			N_FILES=`ls -1q workspace/model/*SAEHD_* | wc -l`
 			zip model_SAEHD.zip workspace/model/*SAEHD_* | tqdm --desc added --unit files --unit_scale --total $N_FILES > /dev/null
