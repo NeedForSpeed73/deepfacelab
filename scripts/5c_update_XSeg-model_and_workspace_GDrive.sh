65bd70f52b338dd70fa4848ac8072243bb2bd416
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-15 14:16:06 +0200

    Update 5c_update_XSeg-model_and_workspace_GDrive.sh

--- scripts/5c_update_XSeg-model_and_workspace_GDrive.sh
+++ scripts/5c_update_XSeg-model_and_workspace_GDrive.sh
@@ -2,26 +2,16 @@
 #
 WORKSPACE_DIR=$HOME/deepfacelab/workspace
 MODEL_DIR=$WORKSPACE_DIR/model
-WORKSPACE_ID_FILE=$HOME/deepfacelab/workspace.id
-MODEL_XSEG_ID_FILE=$HOME/deepfacelab/model_SAEHD.id
 
 if [ "$#" -gt 0 ]; then
 	printf %"s\n" "Usage: 5c_upload_XSeg-model_GDrive.sh"
 else 
 	if [ -d "$MODEL_DIR" ]; then
 		if [ "$(ls -A $MODEL_XSEG_DIR)" ]; then
-			if [ ! -f "$MODEL_XSEG_ID_FILE" ]; then 
-				read -e -p "Enter <model_XSeg.zip> Google Drive ID: " MODEL_XSEG_ID
-				touch $MODEL_XSEG_ID_FILE
-				echo $MODEL_XSEG_ID > $MODEL_XSEG_ID_FILE
-			else
-				MODEL_XSEG_ID=$(<$MODEL_XSEG_ID_FILE)
-			fi
 			printf %"s\n" "Creating model_XSeg.zip"
 			N_FILES=`ls -1q workspace/model/XSeg_* | wc -l`
 			zip model_XSeg.zip workspace/model/XSeg_* | tqdm --desc added --unit files --unit_scale --total $N_FILES > /dev/null				
-			printf %"s\n" "Using id: " $MODEL_XSEG_ID				
-			./gdrive update $MODEL_XSEG_ID model_XSeg.zip
+			./gdrive upload model_XSeg.zip
 			rm model_XSeg.zip
 		else
 			printf %"s\n" "$MODEL_XSEG_DIR is empty."
@@ -31,18 +21,10 @@ else
 	fi
 	if [ -d "$WORKSPACE_DIR" ]; then
 		if [ "$(ls -A $WORKSPACE_DIR)" ]; then
-			if [ ! -f "$WORKSPACE_ID_FILE" ]; then 
-				read -e -p "Enter <workspace.zip> Google Drive ID: " WORKSPACE_ID
-				touch $WORKSPACE_ID
-				echo $WORKSPACE_ID > $WORKSPACE_ID_FILE
-			else
-				WORKSPACE_ID=$(<$WORKSPACE_ID_FILE)
-			fi
 			printf %"s\n" "Creating workspace.zip"
 			N_FILES=`ls -1q workspace/data_*/* | wc -l`
 			zip -r workspace.zip workspace/data_*/* | tqdm --desc added --unit files --unit_scale --total $N_FILES > /dev/null			
-			printf %"s\n" "Using id: " $WORKSPACE_ID
-			./gdrive update $WORKSPACE_ID workspace.zip
+			./gdrive upload $WORKSPACE_ID workspace.zip
 			rm workspace.zip
 		else
 			printf %"s\n" "$WORKSPACE_DIR is empty."
