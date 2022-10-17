522947c3bda6a5900f108f266b365bff6317834f
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-17 14:29:59 +0200

    Update 4_download_workspace_GDrive.sh

--- scripts/4_download_workspace_GDrive.sh
+++ scripts/4_download_workspace_GDrive.sh
@@ -12,28 +12,34 @@ else
 		touch $WORKSPACE_ID_FILE
 		echo $WORKSPACE_ID > $WORKSPACE_ID_FILE
 		printf %"s\n"
+	else
+		WORKSPACE_ID=$(<$WORKSPACE_ID_FILE)
 	fi
 	if [ ! -f "$MODEL_XSEG_ID_FILE" ]; then 
 		read -e -p "Enter <model_XSeg.zip> Google Drive ID: " MODEL_XSEG_ID
 		touch $MODEL_XSEG_ID_FILE
 		echo $MODEL_XSEG_ID > $MODEL_XSEG_ID_FILE
 		printf %"s\n"
+	else
+		MODEL_XSEG_ID=$(<$MODEL_XSEG_ID_FILE)
 	fi
 	if [ ! -f "$MODEL_SAEHD_ID_FILE" ]; then 
 		read -e -p "Enter <model_SAEHD.zip> Google Drive ID: " MODEL_SAEHD_ID
 		touch $MODEL_SAEHD_ID_FILE
 		echo $MODEL_SAEHD_ID > $MODEL_SAEHD_ID_FILE
 		printf %"s\n"
+	else
+		MODEL_SAEHD_ID=$(<$MODEL_SAEHD_ID_FILE)
 	fi
-	./gdrive download $WORKSPACE_ID
+	gdown --id $WORKSPACE_ID
 	N_FILES=`unzip -l workspace.zip | tail -n 1 | xargs echo -n | cut -d' ' -f2`
 	unzip -o workspace.zip | tqdm --desc extracted --unit files --unit_scale --total $N_FILES > /dev/null
 	rm workspace.zip
-	./gdrive download $MODEL_XSEG_ID
+	gdown --id $MODEL_XSEG_ID
 	N_FILES=`unzip -l model_XSeg.zip | tail -n 1 | xargs echo -n | cut -d' ' -f2`
 	unzip -o model_XSeg.zip | tqdm --desc extracted --unit files --unit_scale --total $N_FILES > /dev/null
 	rm model_XSeg.zip
-	./gdrive download $MODEL_SAEHD_ID
+	gdown --id $MODEL_SAEHD_ID
 	N_FILES=`unzip -l model_SAEHD.zip | tail -n 1 | xargs echo -n | cut -d' ' -f2`
 	unzip -o model_SAEHD.zip | tqdm --desc extracted --unit files --unit_scale --total $N_FILES > /dev/null
 	rm model_SAEHD.zip
