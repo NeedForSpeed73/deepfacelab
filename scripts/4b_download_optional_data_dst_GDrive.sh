1ffff0a9ed01220cd9f6a1fcc925bf60d27f06c3
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-19 14:34:56 +0200

    Update 4b_download_optional_data_dst_GDrive.sh

--- scripts/4b_download_optional_data_dst_GDrive.sh
+++ scripts/4b_download_optional_data_dst_GDrive.sh
@@ -12,15 +12,9 @@ else
 	else
 		DATA_DST_ID=$(<$DATA_DST_ID_FILE)
 	fi
-	FILE_SIZE=$(./gdrive info $DATA_DST_ID | grep Size: | awk -v N=2 '{print $N}')
-	NEEDED_SPACE=$(($FILE_SIZE * 3))
-	FREE_SPACE=$(df -H | grep -wE '/' | awk -v N=4 '{print substr($N, 1, length($N)-1)}')
-	if [ $FREE_SPACE -gt $NEEDED_SPACE ]; then
-	 	gdown --id $DATA_DST_ID
-	 	N_FILES=`unzip -l data_dst.zip | tail -n 1 | xargs echo -n | cut -d' ' -f2`
-	 	unzip -o data_dst.zip | tqdm --desc extracted --unit files --unit_scale --total $N_FILES > /dev/null
-	 	rm data_dst.zip
-	else
-		printf %"s\n" "Not enough space on drive for operation."
-	fi
+	
+	 gdown --id $DATA_DST_ID
+	 N_FILES=`unzip -l data_dst.zip | tail -n 1 | xargs echo -n | cut -d' ' -f2`
+	 unzip -o data_dst.zip | tqdm --desc extracted --unit files --unit_scale --total $N_FILES > /dev/null
+	 rm data_dst.zip
 fi
