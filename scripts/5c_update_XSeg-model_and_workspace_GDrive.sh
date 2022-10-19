db608f48ec0ed839ecfb402bc51aa69431f33cc4
NeedForSpeed73 <need_for_speed@libero.it>
2022-10-19 16:30:15 +0200

    Update 5c_update_XSeg-model_and_workspace_GDrive.sh

--- scripts/5c_update_XSeg-model_and_workspace_GDrive.sh
+++ scripts/5c_update_XSeg-model_and_workspace_GDrive.sh
@@ -13,13 +13,16 @@ else
 			if [ -f "$WORKSPACE_ID_FILE" ]; then
 				WORKSPACE_ID=$(<$WORKSPACE_ID_FILE)
 			else
-				read -e -p "Enter <workspace.zip> Google Drive ID or "new" for new file: " WORKSPACE_ID
+				read -e -p "Enter <workspace.zip> Google Drive Link or "new" for new file: " WORKSPACE_LINK
+				if [[ WORKSPACE_LINK != "new" ]]; then
+					WORKSPACE_ID="$(echo "$WORKSPACE_LINK" | awk -F"/" '{print $6}')"
+				fi
 				printf %"s\n"
 			fi
 			printf %"s\n" "Creating workspace.zip"
 			N_FILES=`ls -1q workspace/data_*/* | wc -l`
 			zip -r workspace.zip workspace/data_*/* | tqdm --desc added --unit files --unit_scale --total $N_FILES > /dev/null
-			if [[ WORKSPACE_ID == "new" ]]; then
+			if [[ WORKSPACE_ID_LINK == "new" ]]; then
 				printf %"s\n" "Creating new GDrive file workspace.zip in main directory."
 				./gdrive upload workspace.zip
 				
@@ -39,13 +42,15 @@ else
 			if [ -f "MODEL_XSEG_ID_FILE" ]; then
 				MODEL_XSEG_ID=$(<$MODEL_XSEG_ID_FILE)
 			else
-				read -e -p "Enter <model_XSeg.zip> Google Drive ID or "new" for new file: " MODEL_XSEG_ID
+				read -e -p "Enter <model_XSeg.zip> Google Drive Link or "new" for new file: " MODEL_XSEG_LINK
+				if [[ MODEL_XSEG_LINK != "new" ]]; then
+					MODEL_XSEG_ID="$(echo "$MODEL_XSEG_LINK" | awk -F"/" '{print $6}')"
 				printf %"s\n"
 			fi
 			printf %"s\n" "Creating model_XSeg.zip"
 			N_FILES=`ls -1q workspace/model/XSeg_* | wc -l`
 			zip model_XSeg.zip workspace/model/XSeg_* | tqdm --desc added --unit files --unit_scale --total $N_FILES > /dev/null
-			if [[ MODEL_XSEG_ID == "new" ]]; then
+			if [[ MODEL_XSEG_LINK == "new" ]]; then
 				printf %"s\n" "Creating new GDrive file model_XSeg.zip in main directory."
 				./gdrive upload model_XSeg.zip
 				
